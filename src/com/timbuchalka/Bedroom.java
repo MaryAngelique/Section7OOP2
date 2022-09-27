123d3f1368d908097fa7610fd5ded619ee7b1493
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-27 10:53:09 -0400

    Implemented getters in the Bedroom class

--- src/com/timbuchalka/Bedroom.java
+++ src/com/timbuchalka/Bedroom.java
@@ -12,6 +12,7 @@ public class Bedroom {
     private Lamp lamp;
 
     public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp) {
+
         this.name = name;
         this.wall1 = wall1;
         this.wall2 = wall2;
@@ -20,5 +21,16 @@ public class Bedroom {
         this.ceiling = ceiling;
         this.bed = bed;
         this.lamp = lamp;
+
     }
+
+    public Lamp getLamp() {
+        return this.lamp;
+    }
+
+    public void makeBed() {
+        System.out.println("Bedroom -> Making bed");
+        bed.make();
+    }
+
 }
