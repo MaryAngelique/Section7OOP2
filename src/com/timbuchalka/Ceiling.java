fda6fbfdabfaaeffa7b28ee7ef74bd24128bd9d9
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-27 23:55:03 -0400

    Implemented Ceiling constructor

--- src/com/timbuchalka/Ceiling.java
+++ src/com/timbuchalka/Ceiling.java
@@ -5,4 +5,8 @@ public class Ceiling {
     private int height;
     private int paintedColor;
 
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
 }
