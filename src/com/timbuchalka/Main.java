a64b1b0d1705927f749a5643a8645560d0f7038a
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-28 12:42:59 -0400

    Implemented Ceiling class and integrated it into Main class

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -26,4 +26,11 @@ public class Main {
     // then add at least one method to hide the object e.g. not using a getter
     // but to access the object used in composition within the main class
 
+    Wall wall1 = new Wall("West");
+    Wall wall2 = new Wall("East");
+    Wall wall3 = new Wall("South");
+    Wall wall4 = new Wall("North");
+
+    Ceiling ceiling = new Ceiling(12, 55);
+
 }
