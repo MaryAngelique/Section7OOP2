3f8af31e82848bbc9bf61f8942a960e84b7599ad
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-27 23:54:24 -0400

    Implemented Ceiling class

---
+++ src/com/timbuchalka/Ceiling.java
@@ -0,0 +1,8 @@
+package com.timbuchalka;
+
+public class Ceiling {
+
+    private int height;
+    private int paintedColor;
+
+}
--- src/com/timbuchalka/Wall.java
+++ src/com/timbuchalka/Wall.java
@@ -2,35 +2,17 @@ package com.timbuchalka;
 
 public class Wall {
 
-    private String name;
-    private Wall wall1;
-    private Wall wall2;
-    private Wall wall3;
-    private Wall wall4;
-    private Ceiling ceiling;
-    private Bed bed;
-    private Lamp lamp;
+    private String direction;
 
-    public Wall(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp) {
+    public Wall(String direction) {
 
-        this.name = name;
-        this.wall1 = wall1;
-        this.wall2 = wall2;
-        this.wall3 = wall3;
-        this.wall4 = wall4;
-        this.ceiling = ceiling;
-        this.bed = bed;
-        this.lamp = lamp;
+        this.direction = direction;
 
     }
 
-    public Lamp getLamp() {
-        return this.lamp;
-    }
+    public String getDirection() {
 
-    public void makeBed() {
-        System.out.println("Bedroom -> Making bed");
-        bed.make();
-    }
+        return direction;
 
+    }
 }
