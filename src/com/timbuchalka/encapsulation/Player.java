ec9b1b33ad23368b2ecfcad556a3d316e585ee84
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-29 13:02:25 -0400

    Implemented loseHealth method to reduce the player's health

--- src/com/timbuchalka/encapsulation/Player.java
+++ src/com/timbuchalka/encapsulation/Player.java
@@ -6,4 +6,11 @@ public class Player {
     public int health;
     public String weapon;
 
+    public void loseHealth(int damage) {
+        this.health = this.health - damage;
+        if(this.health <=0) {
+            System.out.println("Player knocked out");
+        }
+    }
+
 }
