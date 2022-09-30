437dd3e51aa08891da4e54ba079181f0cc356662
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-30 10:44:36 -0400

    Implemented and if-else statement for the printer toner level

--- src/com/timbuchalka/challenge/Printer.java
+++ src/com/timbuchalka/challenge/Printer.java
@@ -8,6 +8,13 @@ public class Printer {
 
     public Printer(int tonerLevel, boolean duplex) {
 
+        if(tonerLevel >-1 && tonerLevel <=100) {
+            this.tonerLevel = tonerLevel;
+
+        } else {
+            this.tonerLevel = -1;
+        }
+
         this.duplex = duplex;
         this.pagesPrinted = 0;
 
