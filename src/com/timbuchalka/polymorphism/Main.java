aa425bc521388e839146d97b7dfa6cd20674be1c
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-01 21:31:44 -0400

    Implemented Jaws class

--- src/com/timbuchalka/polymorphism/Main.java
+++ src/com/timbuchalka/polymorphism/Main.java
@@ -17,6 +17,17 @@ class Movie {
 
 }
 
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
