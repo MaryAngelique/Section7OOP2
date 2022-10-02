96557628c96c14e0f7d7456996965cea6389c191
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-01 22:10:25 -0400

    Implemented Star Wars class with an override method

--- src/com/timbuchalka/polymorphism/Main.java
+++ src/com/timbuchalka/polymorphism/Main.java
@@ -56,6 +56,17 @@ class MazeRunner extends Movie {
 
 
 
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial Forces try to take over the universe";
+    }
+}
+
 
 public class Main {
 
