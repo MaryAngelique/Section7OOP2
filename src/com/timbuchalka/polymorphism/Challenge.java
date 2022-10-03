ffe81f61364c798d9c24388c3eaca2f8f632ffed
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-03 11:29:40 -0400

    Implemented start engine, accelerate, and brake with an override method

--- src/com/timbuchalka/polymorphism/Challenge.java
+++ src/com/timbuchalka/polymorphism/Challenge.java
@@ -88,6 +88,20 @@ class Ford extends Car {
         super(cylinders, name);
     }
 
+    @Override
+    public String startEngine() {
+        return "Ford -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Ford -> brake()";
+    }
 }
 
 
