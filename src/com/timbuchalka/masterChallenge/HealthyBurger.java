58c6c12ad4795b31d0fd6ec95274135ade3995ec
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-04 23:41:15 -0400

    Implemented 2 add health addition methods

--- src/com/timbuchalka/masterChallenge/HealthyBurger.java
+++ src/com/timbuchalka/masterChallenge/HealthyBurger.java
@@ -8,4 +8,24 @@ public class HealthyBurger extends Hamburger {
     private String healthyExtra2Name;
     private double healthyExtra2Price;
 
+    public HealthyBurger(String meat, double price) {
+
+        super("Healthy", meat, price, "Brown rye");
+
+    }
+
+    public void addHealthAddition1(String name, double price) {
+
+        this.healthyExtra1Name = name;
+        this.healthyExtra1Price = price;
+
+    }
+
+    public void addHealthAddition2(String name, double price) {
+
+        this.healthyExtra2Name = name;
+        this.healthyExtra2Price = price;
+
+    }
+
 }
